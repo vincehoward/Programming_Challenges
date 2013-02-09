94d6cd3e40d28cc37d644dbed68ba4efa1f2351f
Vince Howard <hincevoward@gmail.com>
2013-02-08 23:05:42 -0500

    Signed-off-by: Vince Howard <hincevoward@gmail.com>

--- haskell/PE/PE2.hs
+++ haskell/PE/PE2.hs
@@ -1,4 +1,22 @@
+-- ProjectEuler 2
+-- The commented code is code to be run in ghci
+
 fibs :: [Integer]
 fibs = 1 : 2 : zipWith (+) fibs (tail fibs)
 
+
+-- Solution 1
+-- Requires fibs from above
+
 -- sum (filter (\x -> x <= 4000000 && x `mod` 2 == 0) ((take 1000 fibs)))
+
+
+-- solution 3 
+-- Requires fibs from above
+
+-- sum $ takeWhile (<= 4000000) $ filter even fibs
+
+
+-- solution 3
+
+-- sum . takeWhile (<= 4000000) . filter even $ fibs
